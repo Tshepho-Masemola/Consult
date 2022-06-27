80f052eeda4ea7338b178e6600e2446085331413
Tshepho.Masemola <mosh.nwa@gmail.com>
2022-06-27 20:38:33 +0200

    update database code

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,7 @@ public class Main {
                     break;
                 case 2:
                     Place place = new Place();
-                    place.printCustomers();
+                    place.printCustomersToFile();
                     break;
                 case 3:
                     break;
